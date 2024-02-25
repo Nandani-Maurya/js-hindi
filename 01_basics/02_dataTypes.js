a83508de8db0e097c19a30429c80422b4eb1ab05
Raj Nandani Maurya <147945705+Nandani-Maurya@users.noreply.github.com>
2024-02-25 07:05:23 +0000

    datatype done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version (latest version of javascript)
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3);   //code readability should be high
+
+let name= "hitesh"  //string
+let age = 18; //number
+let isLoggedIn = false
+//let state = null;   //it is empty
+//let state;       //value not assigned
+
+
+//number => 2 to the power of 53
+//bigint => used for large numbers
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined => 
+//symbol => components uniqueness in react
+
+//object  ///non primitive datatypes
+
+console.log(typeof null);    //object
+console.log(typeof undefined); //undefined
